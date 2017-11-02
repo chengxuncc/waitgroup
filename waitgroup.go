498b959993504f6a568fbff40f725876e10cbcf8
hzxuzhonghu <xuzhonghu@huawei.com>
2017-11-02 13:38:26 +0800

    fix golint error

--- waitgroup.go
+++ waitgroup.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 package waitgroup
 
 import (
@@ -5,12 +21,23 @@ import (
 	"sync/atomic"
 )
 
+// A WaitGroup waits for a collection of goroutines to finish.
+// The main goroutine calls Add to set the number of
+// goroutines to wait for. Then each of the goroutines
+// runs and calls Done when finished. At the same time,
+// Wait can be used to block until all goroutines have finished.
+//
+// A WaitGroup must not be copied after first use.
 type WaitGroup struct {
 	m       sync.Mutex
 	counter int32
 	ch      chan struct{}
+	sync.WaitGroup
 }
 
+// Add adds delta, which may be negative, to the WaitGroup counter.
+// If the counter becomes zero, all goroutines blocked on Wait are released.
+// If the counter goes negative, Add panics.
 func (wg *WaitGroup) Add(delta int) {
 	v := atomic.AddInt32(&wg.counter, int32(delta))
 	// Panic if negative
@@ -30,11 +57,13 @@ func (wg *WaitGroup) Add(delta int) {
 
 }
 
+// Done decrements the WaitGroup counter.
 func (wg *WaitGroup) Done() {
 	wg.Add(-1)
 
 }
 
+// Wait blocks until the WaitGroup counter is zero.
 func (wg *WaitGroup) Wait() {
 	if atomic.LoadInt32(&wg.counter) == 0 {
 		return
